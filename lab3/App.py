b05474874039d75587ef1c62b6ff77dcfe12a076
Vladyslav Netetskyi <93187192+vlad-netetskyi@users.noreply.github.com>
2021-11-26 14:53:08 +0200

    lab3 Start to learn Flask
    lab3 Start to learn Flask

---
+++ lab3/App.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+
+class App:
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(App, cls).__new__(cls)
+            cls.instance.__app = Flask(__name__)
+        return cls.instance
+
+    def getApp(self):
+        return self.__app
+
+    @staticmethod
+    def getMenu():
+        return {
+            "/": "Home",
+            "/about": "About me",
+            "/achievements": "Achievements"
+        }
